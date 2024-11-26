88fef44892f27a0a12ff13d1449f7c088a16f5df
Anthony941-blip <2023260043@ub.edu.bz>
2024-11-26 16:03:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -95,20 +95,6 @@ document.addEventListener('keyup', function(e) {
 });
 
 
-document.addEventListener('keyup', function(e) {
-    if (e.key === 'ArrowLeft') {
-        leftPressed = false;
-    } else if (e.key === 'ArrowRight') {
-        rightPressed = false;
-    } else if (e.key === 'ArrowUp') {
-        upPressed = false;
-    } else if (e.key === 'ArrowDown') {
-        downPressed = false; // Stop charging when down is released
-    } else if (e.key === ' ') {
-        spacePressed = false; // Space bar released
-    }
-});
-
 // Timer logic
 function startTimer() {
     timerInterval = setInterval(function() {
